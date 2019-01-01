afe71ef40d5cd27649f2c33a23bc462aa2bcb8bc
Ahmed Essam <ahmedessamahmedmohamed@gmail.com>
2019-01-01 22:05:21 +0200

    adding question 6 code

--- second_final_question5/states.php
+++ second_final_question5/states.php
@@ -1,4 +1,23 @@
 <?php
+// states.php file
+
+$states = "Mississippi Alabama Taxas Massachusetts Kansas";
+// xas
+
+
+$states_string_to_array = explode(' ', $states);
+
+$states_array = [];
+
+for($x = 0; $x < count($states_string_to_array); $x++) {
+    if (preg_match('/xas$/', $states_string_to_array[$x]))
+        $states_array[0] = $states_string_to_array[$x];
+
+
+    if(preg_match('/K[\s\S]*s/i', $states_string_to_array[$x]))
+        $states_array[1] = $states_string_to_array[$x];
+}
+
 
 
 
